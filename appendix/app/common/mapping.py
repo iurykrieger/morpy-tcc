a064d6810a6120a8e598492ab7bde7f9da7d1f2a
Iury Krieger <iurykrieger96@gmail.com>
2017-10-29 13:26:29 -0200

    rebase

--- appendix/app/common/mapping.py
+++
@@ -1,8 +0,0 @@
-TYPE_MAPPING = {
-    'unicode': 'string',
-    'string': 'string',
-    'integer': 'integer'
-}
-
-def get_synonymous(term):
-    return TYPE_MAPPING[term] if term in TYPE_MAPPING else term
